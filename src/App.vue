67ed6e3e852af4c47b5b08a8e2ef5c7a773d8c60
Luke O'Leary <luke.oleary1@gmail.com>
2024-01-15 20:05:01 +0000

    feat: inital vue commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Allerta+Stencil&display=swap");
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
