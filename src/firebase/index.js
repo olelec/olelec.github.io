a97e54a03eb39d8a08a27ffc23a916e084596df7
Luke O'Leary <luke.oleary1@gmail.com>
2025-02-27 17:10:29 +0000

    feat: added analytics

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { initializeAnalytics } from "firebase/analytics";
 import { getFirestore, collection } from "firebase/firestore";
 import firebaseConfig from "./firebaseConfig";
 
@@ -6,6 +7,8 @@ export const firebaseApp = initializeApp(firebaseConfig);
 
 // used for the firestore refs
 const db = getFirestore(firebaseApp);
+
+initializeAnalytics(firebaseApp);
 
 // here we can export reusable database references
 export const todosRef = collection(db, "todos");
